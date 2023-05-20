80a63804a03fd3a42d9bbc19886de8d6897d6129
Glucose-180 <Glucose180@163.com>
2023-05-20 09:14:57 +0800

    Initialize DNN-x86 test files

---
+++ custom_cpu/riscv32/DNN-x86/DNN.c
@@ -0,0 +1,102 @@
+#include "DNN.h"
+
+static inline int mul(short a, short b)
+{
+#ifdef SOFT_MUL
+	int ans = mul_ll(a, b);
+#else
+	int ans = a * b;
+#endif
+	return ans;
+}
+
+static inline int div(int x, int y)
+{
+    int q = 0;
+    while (x >= y) {
+        q++;
+        x -= y;
+    }
+
+    return q;
+}
+
+struct mem_addr addr;
+struct size_vec4 rd_size = {RD_SIZE_D0, RD_SIZE_D1, RD_SIZE_D2, RD_SIZE_D3};
+struct size_vec4 wr_size = {WR_SIZE_D0, WR_SIZE_D1, WR_SIZE_D2, WR_SIZE_D3};
+struct size_vec4 weight_size = {WEIGHT_SIZE_D0, WEIGHT_SIZE_D1, WEIGHT_SIZE_D2, WEIGHT_SIZE_D3};
+
+struct size_vec4 conv_size;
+
+void convolution()
+{
+	short *in = (short *)addr.rd_addr;
+	short *weight = (short *)addr.weight_addr;
+	short *out = (short *)addr.wr_addr;
+
+	unsigned output_offset = 0;
+	unsigned input_offset = 0;
+
+	unsigned input_fm_w = rd_size.d3;
+	unsigned input_fm_h = rd_size.d2;
+
+	unsigned pad = KERN_ATTR_CONV_PAD;
+	unsigned pad_len = pad << 1;
+
+	unsigned conv_out_w = rd_size.d3 - weight_size.d3 + pad_len;
+	unsigned conv_out_h = rd_size.d2 - weight_size.d2 + pad_len;
+
+	unsigned stride = KERN_ATTR_CONV_STRIDE;
+
+	conv_out_w = div(conv_out_w, stride);
+	conv_out_h = div(conv_out_h, stride);
+
+	conv_out_w++;
+	conv_out_h++;
+
+	conv_size.d0 = wr_size.d0;
+	conv_size.d1 = wr_size.d1;
+	conv_size.d2 = conv_out_h;
+	conv_size.d3 = conv_out_w;
+
+	//TODO: Please add your implementation here
+}
+
+void pooling()
+{
+	short *out = (short *)addr.wr_addr;
+
+	unsigned output_offset = 0;
+	unsigned input_offset = 0;
+
+	unsigned input_fm_w = conv_size.d3;
+	unsigned input_fm_h = conv_size.d2;
+
+	unsigned pad = KERN_ATTR_POOL_PAD;
+	unsigned pad_len = pad << 1;
+
+	unsigned pad_w_test = conv_size.d3 - KERN_ATTR_POOL_KERN_SIZE;
+	unsigned pad_h_test = conv_size.d2 - KERN_ATTR_POOL_KERN_SIZE;
+
+	unsigned pool_out_w = pad_w_test + pad_len;
+	unsigned pool_out_h = pad_h_test + pad_len;
+
+	unsigned stride = KERN_ATTR_POOL_STRIDE;
+
+	unsigned pad_w_test_remain = pad_w_test - mul(div(pad_w_test, stride), stride);
+	unsigned pad_h_test_remain = pad_h_test - mul(div(pad_h_test, stride), stride);
+
+	pool_out_w = div(pool_out_w, stride);
+	pool_out_h = div(pool_out_h, stride);
+	pool_out_w++;
+	pool_out_h++;
+
+	if ((!pad) && (pad_w_test_remain || pad_h_test_remain))
+	{
+		pool_out_w++;
+		pool_out_h++;
+	}
+
+	//TODO: Please add your implementation here
+
+}
