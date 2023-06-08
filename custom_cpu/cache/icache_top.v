9fe48d528af084a17f741233d535581556f7e51c
Glucose-180 <Glucose180@163.com>
2023-06-08 19:31:18 +0800

    TODO: Recv and refill

--- custom_cpu/cache/icache_top.v
+++ custom_cpu/cache/icache_top.v
@@ -2,7 +2,7 @@
 
 `define CACHE_SET	8
 `define CACHE_WAY	4
-`define LOG_2WAY	2
+`define LOG_2WAY	2	/* Width of order of every way */
 `define TAG_LEN		24
 `define LINE_LEN	256
 `define ADDR_WIDTH	3
@@ -58,18 +58,20 @@ module icache_top (
 	 the way whose order is 3 will be replaced. */
 	reg [`CACHE_SET * `CACHE_WAY * `LOG_2WAY - 1:0] Order;
 
-	wire [`CACHE_WAY * `LOG_2WAY - 1:0] Order_at_raddr, Order_at_waddr;
+	reg [31:0] CAR;	/* CPU address reg */
+
+	wire [`CACHE_WAY * `LOG_2WAY - 1:0] Order_at_raddr,
+		Order_at_waddr, Order_Offset;
 	wire [`CACHE_WAY - 1:0] Valid_at_raddr, Valid_at_waddr;
 
-	wire [23:0] Addr_Tag;
-	wire [2:0] Addr_Index;
-	wire [4:0] Addr_Offset;
+	wire [`TAG_LEN - 1:0] Addr_Tag;
+	wire [`ADDR_WIDTH - 1:0] Addr_Index;
+	wire [31 - `TAG_LEN - `ADDR_WIDTH:0] Addr_Offset;	/* 5 bits */
 
 	/* Write-enable signal of every way */
 	wire [`CACHE_WAY - 1:0] Array_wen;
 	/* Address of all ways */
-	reg [`ADDR_WIDTH - 1:0] Array_waddr;
-	wire [`ADDR_WIDTH - 1:0] Array_raddr;
+	wire [`ADDR_WIDTH - 1:0] Array_addr;
 
 	/* Data read from every way:
 	 255~0 for way 0, ..., 1023~768 for way 3. */
@@ -90,6 +92,13 @@ module icache_top (
 
 	/* Hit flag for every way at certain address */
 	wire [`CACHE_WAY - 1:0] Flag_Hit;
+	/* Miss flag, used to decide which way will be refilled */
+	wire [`CACHE_WAY - 1:0] Flag_Miss;
+	/* Target flag, used to point which way is hit or to be refilled */
+	wire [`CACHE_WAY - 1:0] Flag_Target;
+
+	/* Order of target block, hit or to be refilled */
+	wire [`LOG_2WAY - 1:0] Order_of_Target;
 
 	/* For FSM */
 	reg [4:0] current_state, next_state;
@@ -97,16 +106,15 @@ module icache_top (
 	/* Virtual init state */
 	reg IFR;
 
-	assign Valid_at_raddr = Order[{ Array_raddr,`LOG_2WAY'd0 } +: `CACHE_WAY],
-		Valid_at_waddr = Valid[{ Array_waddr,`LOG_2WAY'd0 } +: `CACHE_WAY];
-	assign Order_at_raddr = Order[{ Array_raddr,3'd0 } +: 8 ],
-		Order_at_waddr = Order[{ Array_waddr,3'd0 } +: 8];
+	assign Valid_at_raddr = Order[{ Array_addr,`LOG_2WAY'd0 } +: `CACHE_WAY],
+		Order_at_raddr = Order[{ Array_addr,3'd0 } +: (`CACHE_WAY * `LOG_2WAY) ];
+		/* Array_addr * `CACHE_WAY * `LOG_2WAY */
 	
-	assign Addr_Tag = from_cpu_inst_req_addr[31:8],
-		Addr_Index = from_cpu_inst_req_addr[7:5],
-		Addr_Offset = from_cpu_inst_req_addr[4:0];
+	assign { Addr_Tag,Addr_Index,Addr_Offset } = (
+		current_state == s_WAIT ? from_cpu_inst_req_addr : CAR
+	);
 
-	assign Array_raddr = Addr_Index;
+	assign Array_addr = Addr_Index;
 
 	assign Flag_Hit = Valid_at_raddr & {
 		Addr_Tag == Tag_r[(3 * `TAG_LEN) +: `TAG_LEN],
@@ -115,6 +123,21 @@ module icache_top (
 		Addr_Tag == Tag_r[(0 * `TAG_LEN) +: `TAG_LEN]
 	};
 
+	/* Just find the first way whose order is 3 */
+	assign Flag_Miss[0] = &Order_at_raddr[(0 * `LOG_2WAY) +: `LOG_2WAY],
+		Flag_Miss[1] = (
+			!Flag_Miss[0] &&
+			&Order_at_raddr[(1 * `LOG_2WAY) +: `LOG_2WAY]
+		),
+		Flag_Miss[2] = (
+			!Flag_Miss[0] && !Flag_Miss[1] &&
+			&Order_at_raddr[(2 * `LOG_2WAY) +: `LOG_2WAY]
+		),
+		Flag_Miss[3] = (
+			!Flag_Miss[0] && !Flag_Miss[1] && Flag_Miss[2] &&
+			&Order_at_raddr[(3 * `LOG_2WAY) +: `LOG_2WAY]
+		);
+
 	assign Block_Hit = (
 		{`LINE_LEN{Flag_Hit[3]}} & Data_r[(3 * `LINE_LEN) +: `LINE_LEN] |
 		{`LINE_LEN{Flag_Hit[2]}} & Data_r[(2 * `LINE_LEN) +: `LINE_LEN] |
@@ -122,15 +145,39 @@ module icache_top (
 		{`LINE_LEN{Flag_Hit[0]}} & Data_r[(0 * `LINE_LEN) +: `LINE_LEN]
 	);
 
+	/* Flag_Target is Flag_Hit when cache hit, or Flag_Miss otherwise */
+	assign Flag_Target = (
+		(Flag_Hit == `CACHE_WAY'd0) ? Flag_Miss : Flag_Hit
+	);
+
+	assign Order_of_Target = (
+		{`LOG_2WAY{Flag_Target[3]}} & Order_at_raddr[(3 * `LOG_2WAY) + `LOG_2WAY] |
+		{`LOG_2WAY{Flag_Target[2]}} & Order_at_raddr[(2 * `LOG_2WAY) + `LOG_2WAY] |
+		{`LOG_2WAY{Flag_Target[1]}} & Order_at_raddr[(1 * `LOG_2WAY) + `LOG_2WAY] |
+		{`LOG_2WAY{Flag_Target[0]}} & Order_at_raddr[(0 * `LOG_2WAY) + `LOG_2WAY]
+	);
+
+	/* To modify order of 4 ways at certain address */
+	assign Order_Offset = {
+		{`LOG_2WAY{Flag_Target[3]}} & (-Order_of_Target) |
+		{`LOG_2WAY{Order_at_raddr[(3 * `LOG_2WAY) + `LOG_2WAY] < Order_of_Target}} & `LOG_2WAY'd1,
+		{`LOG_2WAY{Flag_Target[2]}} & (-Order_of_Target) |
+		{`LOG_2WAY{Order_at_raddr[(2 * `LOG_2WAY) + `LOG_2WAY] < Order_of_Target}} & `LOG_2WAY'd1,
+		{`LOG_2WAY{Flag_Target[1]}} & (-Order_of_Target) |
+		{`LOG_2WAY{Order_at_raddr[(1 * `LOG_2WAY) + `LOG_2WAY] < Order_of_Target}} & `LOG_2WAY'd1,
+		{`LOG_2WAY{Flag_Target[0]}} & (-Order_of_Target) |
+		{`LOG_2WAY{Order_at_raddr[(0 * `LOG_2WAY) + `LOG_2WAY] < Order_of_Target}} & `LOG_2WAY'd1
+	};
+
 	data_array Darray[`CACHE_WAY - 1:0] (
 		/* Only wen and rdata are separate for 4 ways */
-		.clk(clk), .waddr(Array_waddr), .raddr(Array_raddr),
+		.clk(clk), .waddr(Array_waddr), .raddr(Array_addr),
 		.wen(Array_wen), .wdata(Data_w), .rdata(Data_r)
 	);
 	/* Only wdata and rdata are separate for Darray and Tarray */
 	tag_array Tarray[`CACHE_WAY - 1:0] (
 		/* Only wen and rdata are separate for 4 ways */
-		.clk(clk), .waddr(Array_waddr), .raddr(Array_raddr),
+		.clk(clk), .waddr(Array_waddr), .raddr(Array_addr),
 		.wen(Array_wen), .wdata(Tag_w), .rdata(Tag_r)
 	);
 
@@ -180,6 +227,12 @@ module icache_top (
 		/* To yield a virtual initial state */
 	end
 
+	/* CAR */
+	always @ (posedge clk) begin
+		if (current_state == s_WAIT && next_state != s_WAIT)
+			CAR <= from_cpu_inst_req_addr;
+	end
+
 	/* VDR */
 	always @ (posedge clk) begin
 		if (next_state == s_DONE) begin
@@ -190,6 +243,28 @@ module icache_top (
 				;// TODO: after refilling
 		end
 	end
+
+	/* Order */
+	always @ (posedge clk) begin
+		if (rst)
+			Order <= {(`CACHE_SET * `CACHE_WAY * `LOG_2WAY){1'd1}};
+		else if (current_state == s_WAIT && next_state != s_WAIT)
+			/* Renew order */
+			Order <= Order + (
+				{ {((`CACHE_SET - 1) * `CACHE_WAY * `LOG_2WAY){1'd0}},Order_Offset }
+					<< { Array_addr,3'd0 }	/* Array_addr * `CACHE_WAY * `LOG_2WAY */
+			);
+	end
+
+	/* Connect to CPU */
+	assign to_cpu_inst_req_ready = (current_state == s_WAIT),
+		to_cpu_cache_rsp_valid = (current_state == s_DONE),
+		to_cpu_cache_rsp_data = VDR;
+	
+	/* Connect to main memory */
+	assign to_mem_rd_req_valid = (current_state == s_LOAD),
+		to_mem_rd_rsp_ready = (current_state == s_RECV),
+		to_mem_rd_req_addr = { Addr_Tag,Addr_Index,{(31 - `TAG_LEN - `ADDR_WIDTH){1'd0}} };
 
 endmodule
 
