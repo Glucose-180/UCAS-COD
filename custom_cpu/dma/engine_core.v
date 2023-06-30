c91d1a26dc8db9e4799648799758d6050e155c09
Glucose-180 <Glucose180@163.com>
2023-06-30 23:23:08 +0800

    Try divide DMA into two engines. No test!

--- custom_cpu/dma/engine_core.v
+++ custom_cpu/dma/engine_core.v
@@ -70,18 +70,22 @@ module engine_core #(
 
 	reg [26:0] Burst_ymr;	/* Counter of Burst */
 	reg [4:0] Send_ymr;		/* Counter of sending of every Burst */
-	reg [31:0] sub_ptr;		/* Points to the address of current Burst */
+	reg [31:0] lsub_ptr, ssub_ptr;		/* Points to the address of current Burst */
 
 	reg [31:0] FFR;	/* FIFO reg, which holds data read from it */
+	reg [15:0] FIFO_ymr;	/* FIFO counter */
 
-	reg [5:0] current_state, next_state;
+	reg [3:0] load_state, load_next;
+	reg [3:0] store_state, store_next;
 	reg IFR;	/* Initial flag reg */
 
 	reg EFR;	/* Error flag reg. For DEBUG! */
 
 	/* For FSM */
-	localparam s_WAIT = 6'h1, s_LOAD = 6'h2, s_RECV = 6'h4,
-		s_STOR = 6'h8, s_FFRD = 6'h10, s_SEND = 6'h20;
+	localparam ls_WAIT = 4'h1, ls_LOAD = 4'h2,
+		ls_RECV = 4'h4, ls_DONE = 4'h8;	/* Loading engine */
+	localparam ss_WAIT = 4'h1, ss_STOR = 4'h2,
+		ss_FFRD = 4'h4, ss_SEND = 4'h8;	/* Storing engine */
 
 	/* reg writing signals */
 	localparam w_SRC = 6'b000001, w_DEST = 6'b000010, w_TAIL = 6'b000100,
@@ -90,58 +94,89 @@ module engine_core #(
 	/* FSM: 1 */
 	always @ (posedge clk) begin
 		if (rst)
-			current_state <= s_WAIT;
+			load_state <= ls_WAIT;
 		else
-			current_state <= next_state;
+			load_state <= load_next;
+	end
+	always @ (posedge clk) begin
+		if (rst)
+			store_state <= ss_WAIT;
+		else
+			store_state <= store_next;
 	end
 
 	/* FSM: 2 */
 	always @ (*) begin
-		case (current_state)
-		s_WAIT:
+		case (load_state)
+		ls_WAIT:
 			if (ctrl_stat[0] && head_ptr != tail_ptr &&
 			/* DMA::EN */
 				/*!intr && */dma_size != 32'd0 && !IFR)
-				next_state = s_LOAD;
+				load_next = ls_LOAD;
 			else
-				next_state = s_WAIT;
-		s_LOAD:
+				load_next = ls_WAIT;
+		ls_LOAD:
 			if (rd_req_ready)
-				next_state = s_RECV;
+				load_next = ls_RECV;
 			else
-				next_state = s_LOAD;
-		s_RECV:
-			if (rd_valid && rd_last)
-				next_state = s_STOR;
+				load_next = ls_LOAD;
+		ls_RECV:
+			if (!(rd_valid && rd_last && rd_ready))
+				load_next = ls_RECV;
+			else begin
+				if (Burst_ymr == dma_size[31:5])
+				/* One sub buffer has been finished */
+					load_next = ls_DONE;
+				else
+					load_next = ls_LOAD;
+			end
+		default:	/* ls_DONE, wait for storing engine */
+			if (fifo_is_empty)
+				/* FIFO is empty */
+				load_next = ls_WAIT;
 			else
-				next_state = s_RECV;
-		s_STOR:
+				load_next = ls_DONE;
+		endcase
+	end
+	always @ (*) begin
+		case (store_state)
+		ss_WAIT:
+			if (!IFR && FIFO_ymr[15:3] != 13'd0)
+				/* FIFO_ymr >= 8 */
+				store_next = ss_STOR;
+			else
+				store_next = ss_WAIT;
+		ss_STOR:
 			if (wr_req_ready)
-				next_state = s_FFRD;
+				store_next = ss_FFRD;
 			else
-				next_state = s_STOR;
-		s_FFRD:
+				store_next = ss_STOR;
+		ss_FFRD:
 			if (fifo_rden == 0)
-				next_state = s_SEND;
+				store_next = ss_SEND;
 			else
-				next_state = s_FFRD;
-		default:	/* s_SEND */
+				store_next = ss_FFRD;
+		default:	/* ss_SEND */
 			if (!wr_ready)
-				next_state = s_SEND;
-			else begin	/* wr_ready */
+				store_next = ss_SEND;
+			else begin
 				if (Send_ymr != wr_req_len)
-					/* one burst has not finished */
-					next_state = s_FFRD;
-				else if (Burst_ymr == dma_size[31:5])
-					/* one sub buffer has finished */
-					next_state = s_WAIT;
-				else	/* next sub buffer */
-					next_state = s_LOAD;
+					store_next = ss_FFRD;
+				else
+					store_next = ss_WAIT;
 			end
 		endcase
 	end
 
 	assign intr = ctrl_stat[31];
+
+	/* FIFO_ymr */
+	always @ (posedge clk) begin
+		FIFO_ymr <= FIFO_ymr + (
+			{16{fifo_wen && !fifo_is_full}} & 16'd1 |	/* inc */
+			{16{fifo_rden && !fifo_is_empty}} & ~16'd0	/* dec */
+		);
+	end
 
 	/* IFR */
 	always @ (posedge clk) begin
@@ -171,7 +206,7 @@ module engine_core #(
 			tail_ptr <= 32'd0;
 		else if (reg_wr_en == w_TAIL)
 			tail_ptr <= reg_wr_data;
-		else if (current_state == s_SEND && next_state == s_WAIT)
+		else if (load_state == ls_DONE && load_next == ss_WAIT)
 			/* One sub buffer has been finished */
 			tail_ptr <= { tail_ptr[31:5] + Burst_ymr,5'd0 };
 	end
@@ -198,28 +233,37 @@ module engine_core #(
 			ctrl_stat <= 32'd0;
 		else if (reg_wr_en == w_CTRL)
 			ctrl_stat <= reg_wr_data;
-		else if (current_state == s_SEND && next_state == s_WAIT)
+		else if (load_state == ls_DONE && load_next == ss_WAIT)
 			/* Set interrupt signal */
 			ctrl_stat <= { 1'd1,ctrl_stat[30:0] };
 	end
 
-	/* sub_ptr */
+	/* lsub_ptr */
 	always @ (posedge clk) begin
 		if (rst)
-			sub_ptr <= 32'd0;
-		else if (next_state == s_LOAD) begin
-			if (current_state == s_WAIT)
+			lsub_ptr <= 32'd0;
+		else if (load_next == ls_LOAD) begin
+			if (load_state == ls_WAIT)
 				/* new sub buffer */
-				sub_ptr <= tail_ptr;
-			else if (current_state == s_SEND)
+				lsub_ptr <= tail_ptr;
 				/* new Burst */
-				sub_ptr <= sub_ptr + 32'd32;
+			else if (load_state == ls_RECV)
+				lsub_ptr <= lsub_ptr + 32'd32;
 		end
+	end
+
+	/* ssub_ptr */
+	always @ (posedge clk) begin
+		if (rst)
+			ssub_ptr <= 32'd0;
+		else if (store_state == ss_SEND && store_next == ss_WAIT)
+			/* new Burst */
+			ssub_ptr <= ssub_ptr + 32'd32;
 	end
 
 	/* FFR */
 	always @ (posedge clk) begin
-		if (current_state == s_FFRD && next_state == s_SEND)
+		if (store_state == ss_FFRD && store_next == ss_SEND)
 			FFR <= fifo_rdata;
 	end
 
@@ -227,35 +271,37 @@ module engine_core #(
 	always @ (posedge clk) begin
 		if (rst || fifo_rden)
 			fifo_rden <= 0;
-		else if (next_state == s_FFRD && fifo_rden == 0)
+		else if (store_next == ss_FFRD && fifo_rden == 0)
 			fifo_rden <= 1;
 	end
 
 	/* Connect to main memory */
-	assign rd_req_addr = src_base + sub_ptr, wr_req_addr = dest_base + sub_ptr,
+	assign rd_req_addr = src_base + lsub_ptr, wr_req_addr = dest_base + ssub_ptr,
 		rd_req_len = 5'd7, wr_req_len = 5'd7,
-		rd_ready = (IFR || current_state == s_RECV),
-		wr_data = FFR, wr_valid = (current_state == s_SEND),
+		/* Only when FIFO is not full can rd_ready be 1 */
+		rd_ready = (IFR || load_state == ls_RECV && !fifo_is_full),
+		wr_data = FFR, wr_valid = (store_state == ss_SEND),
 		wr_last = (wr_valid && Send_ymr == wr_req_len),
-		rd_req_valid = (current_state == s_LOAD), wr_req_valid = (current_state == s_STOR);
+		rd_req_valid = (load_state == ls_LOAD), wr_req_valid = (store_state == ss_STOR);
 
 	/* Connect to FIFO */
 	assign fifo_wdata = rd_rdata,
-		fifo_wen = (current_state == s_RECV && rd_valid && rd_ready);
+		fifo_wen = (load_state == ls_RECV && rd_valid && rd_ready);
 
 	/* Send_ymr */
 	always @ (posedge clk) begin
-		if (current_state == s_STOR)
+		if (store_state == ss_STOR)
+			/* clear */
 			Send_ymr <= 5'd0;
-		else if (current_state == s_SEND && next_state == s_FFRD)
+		else if (store_state == ss_SEND && store_next == ss_FFRD)
 			Send_ymr <= Send_ymr + 5'd1;
 	end
 
 	/* Burst_ymr */
 	always @ (posedge clk) begin
-		if (rst || next_state == s_WAIT)
+		if (rst || load_next == ls_WAIT)
 			Burst_ymr <= 27'd0;
-		else if (current_state != s_LOAD && next_state == s_LOAD)
+		else if (load_state != ls_LOAD && load_next == ls_LOAD)
 			/* Begin a new Burst */
 			Burst_ymr <= Burst_ymr + 27'd1;
 	end
